858189fcea95edcd69861c87a332429ef60c8346
Maxified01 <a.emoh@alustudent.com>
2024-07-07 14:42:40 -0700

    retry

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,13 +9,32 @@ def number_of_subscribers(subreddit):
     """
     number of subscribers
     """
-    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
-
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}  # avoid Too Many Requests error
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        data = response.json()["data"]
+        return data["subscribers"]
     else:
         return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
